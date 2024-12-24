847d86524388f97a2597fa44e923df0e152d52d7
Zin Wai Shine <102810234+zin-wai-shine@users.noreply.github.com>
2024-12-24 15:03:21 +0700

    ...

--- config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('DB_HOST', '127.0.0.1');
-define('DB_NAME', 'vending_machine');
-define('DB_USER', 'root');
-define('DB_PASS', '1234!@#$QWEasz');
-
-define ('BASE_URL', 'https://crm.fulfilmm.com');
-
-
